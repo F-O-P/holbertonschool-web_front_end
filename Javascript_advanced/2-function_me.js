384fb97f657266db1652b72ea7ad31019c90c8c0
F-O-P <tayler.coon@gmail.com>
2023-11-13 10:29:52 -0600

    fixed function and added alert instead of console log

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,17 @@
+function welcomeMessage(fullName) {
+  return function() {
+    alert('Welcome ' + fullName);
+  };
+}
+
+var guillaume = welcomeMessage('Guillaume');
+var alex = welcomeMessage('Alex');
+var fred = welcomeMessage('Fred');
+
+// test in web console by copying and pasting the above
+// code into the console.
+// Execute the following in the console:
+// guillaume();
+// alex();
+// fred();
+// The return should be Welcome Guillaume, Welcome Alex, and Welcome Fred.
